bf03f02fdefa353d4b99ee09ce1b5ed7a709b1a3
Josh Watley <watleyjosh@gmail.com>
2023-06-25 14:16:20 +0100

    Initial Commit Empty Project

---
+++ src/main/java/com/jwtly10/tradedashboard/TradeDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.jwtly10.tradedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwtly10/tradedashboard/TradeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtly10.tradedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
