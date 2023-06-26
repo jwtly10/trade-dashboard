a464e062072ff908ee3b27784f3800b6ee35e23c
Josh Watley <watleyjosh@gmail.com>
2023-06-26 20:21:32 +0100

    Adding custom css and starting stats component

--- vue/trade-dashboard-frontend/src/App.vue
+++ vue/trade-dashboard-frontend/src/App.vue
@@ -1,18 +1,58 @@
 <template>
     <div class="text-center" id="app">
-        <Trades />
+        <div class="container text-center">
+            <div class="row">
+                <div class="row">
+                    <div class="col">
+                        <MoneyStats v-bind:stats="pnl"/>
+                    </div>
+                    <div class="col">
+                        <MoneyStats v-bind:stats="pnl"/>
+                    </div>
+                    <div class="col">
+                        <MoneyStats v-bind:stats="pnl"/>
+                    </div>
+                    <div class="col-12">
+                        <MoneyStats v-bind:stats="stats"/>
+                    </div>
+                </div>
+                <div class="row">
+                    <Trades />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import Trades from './components/Trades.vue'
+import Trades from './components/Trades.vue'
+import MoneyStats from "@/components/MoneyStats.vue";
 
-    export default {
-        name: 'App',
-        components: {
-            Trades
+export default {
+    name: 'App',
+    components: {
+        MoneyStats,
+        Trades
+    },
+    data() {
+        return {
+            pnl: [{
+                header: 'Daily PNL',
+                value: 576.49,
+                percentage: 3.3
+            }],
+            stats: [{
+                header: 'Net Profit / Loss',
+                value: 256.4,
+                percentage: -2.3
+            }, {
+                header: 'Daily Drawdown Breach',
+                value: 10043.3,
+                percentage: -5.0
+            }]
         }
     }
+}
 
 </script>
 
--- vue/trade-dashboard-frontend/src/main.js
+++ vue/trade-dashboard-frontend/src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue'
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
+import '../src/assets/custom.scss'
 
 createApp(App).mount('#app')
