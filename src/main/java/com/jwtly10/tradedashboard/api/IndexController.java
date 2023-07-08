d06d85fa9e72ef249c3c979cda8a71a6357d8fbd
Josh Watley <watleyjosh@gmail.com>
2023-07-08 23:25:13 +0100

    Adding index page

---
+++ src/main/java/com/jwtly10/tradedashboard/api/IndexController.java
@@ -0,0 +1,13 @@
+package com.jwtly10.tradedashboard.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "/index";
+    }
+}
